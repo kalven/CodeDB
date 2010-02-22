b0aef3c07bf381308347aaec7a6859295e9d82f7
Daniel Andersson <kalven@gmail.com>
2010-02-22 23:54:36 +0100

    Added parameters to finder: --subidr and --input

--- finder.cpp
+++ finder.cpp
@@ -16,6 +16,11 @@ namespace bxp = boost::xpressive;
 namespace bip = boost::interprocess;
 namespace bpo = boost::program_options;
 
+bool isprefix(const std::string& big, const std::string& prefix)
+{
+    return big.size() >= prefix.size() && big.compare(0, prefix.size(), prefix) == 0;
+}
+
 std::size_t count_lines(const char* begin, const char* end, const char*& last_line)
 {
     std::size_t count = 0;
@@ -42,48 +47,52 @@ class finder
 {
   public:
     finder(std::string packed,
-           std::string index)
+           std::string index,
+           std::string prefix)
       : m_mapping(packed.c_str(), bip::read_only)
       , m_region(m_mapping, bip::read_only)
+      , m_data(static_cast<const char*>(m_region.get_address()))
+      , m_slice_start(0)
+      , m_slice_end(0)
     {
-        m_data_start = static_cast<const char*>(m_region.get_address());
-        m_data_end = m_data_start + m_region.get_size();
-
-        load_index(index);
+        load_index(index, prefix);
     }
 
     void search(const bxp::cregex& re)
     {
         bxp::cmatch what;
 
-        const char* search_start = m_data_start;
-        while(regex_search(search_start, m_data_end, what, re))
+        if(m_slice_start == m_slice_end)
+            return;
+
+        const char* search_start = m_slice_start;
+        while(regex_search(search_start, m_slice_end, what, re))
         {
-            std::size_t offset = (search_start - m_data_start) +
+            std::size_t offset = (search_start - m_data) +
                 static_cast<std::size_t>(what.position());
 
-            file_range range = find_file_range(m_data_start + offset);
+            file_range range = find_file_range(m_data + offset);
 
             const char* line_start;
             std::size_t line = count_lines(range.m_start,
-                                           m_data_start + offset,
+                                           m_data + offset,
                                            line_start);
             const char* line_end = std::strchr(line_start, 10);
 
-            std::cout << (line+1) << ':' << range.m_path << ':';
+            std::cout << range.m_path << ':' << (line+1) << ':';
             std::cout.write(line_start, line_end - line_start);
             std::cout << '\n';
 
             search_start = line_end + 1;
 
-            if(search_start >= m_data_end)
+            if(search_start >= m_slice_end)
                 break;
         }
     }
 
   private:
 
-    void load_index(std::string path)
+    void load_index(std::string path, std::string prefix)
     {
         std::ifstream input(path.c_str());
         if(!input.is_open())
@@ -106,7 +115,15 @@ class finder
                 std::string file = what[2];
 
                 offset += size;
-                m_index.insert(std::make_pair(offset, file));
+
+                if(isprefix(file, prefix))
+                {
+                    if(m_slice_start == 0)
+                        m_slice_start = m_data + offset - size;
+                    m_slice_end = m_data + offset;
+
+                    m_index.insert(std::make_pair(offset, file));
+                }
             }
         }
     }
@@ -120,7 +137,7 @@ class finder
 
     file_range find_file_range(const char* location)
     {
-        std::size_t offset = location - m_data_start;
+        std::size_t offset = location - m_data;
 
         index_t::const_iterator i = m_index.lower_bound(offset);
         if(i == m_index.end())
@@ -135,8 +152,8 @@ class finder
 
         file_range result;
         result.m_path  = i->second;
-        result.m_start = m_data_start + range_start;
-        result.m_end   = m_data_start + i->first;
+        result.m_start = m_data + range_start;
+        result.m_end   = m_data + i->first;
 
         return result;
     }
@@ -146,8 +163,9 @@ class finder
     bip::file_mapping  m_mapping;
     bip::mapped_region m_region;
     index_t            m_index;
-    const char*        m_data_start;
-    const char*        m_data_end;
+    const char*        m_data;
+    const char*        m_slice_start;
+    const char*        m_slice_end;
 };
 
 int main(int argc, char** argv)
@@ -159,7 +177,9 @@ int main(int argc, char** argv)
         bpo::options_description desc("Options");
         desc.add_options()
             ("help,h", "show this help message")
-            ("ignore-case,i", "ignore case distinctions");
+            ("ignore-case,i", "ignore case distinctions")
+            ("subdir,d", bpo::value<std::string>()->default_value(""), "limit search to a specific sub directory")
+            ("input,b",  bpo::value<std::string>(), "input base name");
 
         bpo::variables_map vm;
         bpo::parsed_options parsed =
@@ -167,10 +187,11 @@ int main(int argc, char** argv)
         bpo::store(parsed, vm);
         bpo::notify(vm);
 
-        if(vm.count("help"))
+        if(vm.count("help") || !vm.count("input"))
         {
-            std::cerr << "Usage: finder [OPTION]... PATTERN...\n"
-                      << "Search for each PATTERN in the db\n\n"
+            std::cerr << "Usage: finder -b BASE [OPTION]... PATTERN...\n"
+                      << "Search for each PATTERN in the db\n"
+                      << "Loads db from BASE.dat and BASE.idx\n\n"
                       << desc << std::endl;
             return 0;
         }
@@ -182,10 +203,13 @@ int main(int argc, char** argv)
         if(vm.count("ignore-case"))
             regex_options = regex_options|bxp::regex_constants::icase;
 
+        std::string subdir = vm["subdir"].as<std::string>();
+        std::string input  = vm["input"].as<std::string>();
+
         std::vector<std::string> patterns =
             bpo::collect_unrecognized(parsed.options, bpo::include_positional);
 
-        finder f("code.dat", "code.idx");
+        finder f(input + ".dat", input + ".idx", subdir);
         for(unsigned i = 0; i != patterns.size(); ++i)
             f.search(bxp::cregex::compile(patterns[i].c_str(), regex_options));
     }
