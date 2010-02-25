0fbd173adaf1a69177945a31ddc8b25b6ec90e62
Daniel Andersson <kalven@gmail.com>
2010-02-25 23:59:10 +0100

    Simplified finder. Now searches through each file region sequentially.

--- finder.cpp
+++ finder.cpp
@@ -3,14 +3,13 @@
 #include <boost/xpressive/xpressive_dynamic.hpp>
 #include <boost/program_options.hpp>
 #include <boost/lexical_cast.hpp>
-#include <boost/next_prior.hpp>
 
 #include <string>
 #include <fstream>
 #include <iostream>
 #include <stdexcept>
-
-#include <map>
+#include <utility>
+#include <vector>
 
 namespace bxp = boost::xpressive;
 namespace bip = boost::interprocess;
@@ -47,52 +46,59 @@ class finder
 {
   public:
     finder(std::string packed,
-           std::string index,
-           std::string prefix)
+           std::string index)
       : m_mapping(packed.c_str(), bip::read_only)
       , m_region(m_mapping, bip::read_only)
       , m_data(static_cast<const char*>(m_region.get_address()))
-      , m_slice_start(0)
-      , m_slice_end(0)
+      , m_data_end(m_data + m_region.get_size())
     {
-        load_index(index, prefix);
+        load_index(index);
     }
 
-    void search(const bxp::cregex& re)
+    void search(const bxp::cregex& re, const std::string& prefix)
     {
         bxp::cmatch what;
 
-        if(m_slice_start == m_slice_end)
+        if(m_data == m_data_end)
             return;
 
-        const char* search_start = m_slice_start;
-        while(regex_search(search_start, m_slice_end, what, re))
+        const char* file_start = m_data;
+        for(index_t::const_iterator i = m_index.begin(); i != m_index.end(); ++i)
         {
-            std::size_t offset = (search_start - m_data) +
-                static_cast<std::size_t>(what.position());
+            const char* file_end = m_data + i->first;
+            const char* search_start = file_start;
 
-            file_range range = find_file_range(m_data + offset);
+            if(isprefix(i->second, prefix))
+            {
+                while(regex_search(search_start, file_end, what, re))
+                {
+                    std::size_t offset = (search_start - m_data) +
+                        static_cast<std::size_t>(what.position());
 
-            const char* line_start;
-            std::size_t line = count_lines(range.m_start,
-                                           m_data + offset,
-                                           line_start);
-            const char* line_end = std::strchr(line_start, 10);
+                    const char* line_start;
+                    std::size_t line = count_lines(file_start,
+                                                   m_data + offset,
+                                                   line_start);
+                    const char* line_end = std::strchr(line_start, 10);
 
-            std::cout << range.m_path << ':' << (line+1) << ':';
-            std::cout.write(line_start, line_end - line_start);
-            std::cout << '\n';
+                    std::cout << i->second << ':' << (line+1) << ':';
+                    std::cout.write(line_start, line_end - line_start);
+                    std::cout << '\n';
 
-            search_start = line_end + 1;
+                    search_start = line_end + 1;
 
-            if(search_start >= m_slice_end)
-                break;
+                    if(search_start >= file_end)
+                        break;
+                }
+            }
+
+            file_start = file_end;
         }
     }
 
   private:
 
-    void load_index(std::string path, std::string prefix)
+    void load_index(std::string path)
     {
         std::ifstream input(path.c_str());
         if(!input.is_open())
@@ -115,57 +121,18 @@ class finder
                 std::string file = what[2];
 
                 offset += size;
-
-                if(isprefix(file, prefix))
-                {
-                    if(m_slice_start == 0)
-                        m_slice_start = m_data + offset - size;
-                    m_slice_end = m_data + offset;
-
-                    m_index.insert(std::make_pair(offset, file));
-                }
+                m_index.push_back(std::make_pair(offset, file));
             }
         }
     }
 
-    struct file_range
-    {
-        std::string m_path;
-        const char* m_start;
-        const char* m_end;
-    };
-
-    file_range find_file_range(const char* location)
-    {
-        std::size_t offset = location - m_data;
-
-        index_t::const_iterator i = m_index.lower_bound(offset);
-        if(i == m_index.end())
-            throw std::runtime_error("Location is out of range");
-
-        index_t::const_iterator next = boost::next(i);
-        if(next != m_index.end() && i->first == offset)
-            i = next;
-
-        std::size_t range_start =
-            i == m_index.begin() ? 0 : boost::prior(i)->first;
-
-        file_range result;
-        result.m_path  = i->second;
-        result.m_start = m_data + range_start;
-        result.m_end   = m_data + i->first;
-
-        return result;
-    }
-
-    typedef std::map<std::size_t, std::string> index_t;
+    typedef std::vector<std::pair<std::size_t, std::string> > index_t;
 
     bip::file_mapping  m_mapping;
     bip::mapped_region m_region;
     index_t            m_index;
     const char*        m_data;
-    const char*        m_slice_start;
-    const char*        m_slice_end;
+    const char*        m_data_end;
 };
 
 int main(int argc, char** argv)
@@ -209,9 +176,9 @@ int main(int argc, char** argv)
         std::vector<std::string> patterns =
             bpo::collect_unrecognized(parsed.options, bpo::include_positional);
 
-        finder f(input + ".dat", input + ".idx", subdir);
+        finder f(input + ".dat", input + ".idx");
         for(unsigned i = 0; i != patterns.size(); ++i)
-            f.search(bxp::cregex::compile(patterns[i].c_str(), regex_options));
+            f.search(bxp::cregex::compile(patterns[i].c_str(), regex_options), subdir);
     }
     catch(const std::exception& e)
     {
